db55d25b766ac13b168c3e53019a5ec3c6b4777f
suraj korde <samir6871korde@gmail.com>
2024-03-05 21:23:11 +0530

    first commit

---
+++ partials/_footer.php
@@ -0,0 +1,3 @@
+<div class="footer container-fluid bg-dark text-light">
+    <p class="text-center py-2 mb-0">Copyright © 2024 Designed by <a href="https://github.com/Suraj6608" target="_blank" rel="noopener noreferrer">@Surajkorde@5719</a></p>
+</div>
